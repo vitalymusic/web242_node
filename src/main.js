c92ce463e9217e7b8711177c5fb1d67af2b13e3f
Vitālijs Korabeļņikovs <vitalijs.korabelnikovs@gmail.com>
2024-11-11 20:04:00 +0200

    NodeJS

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import $ from 'jquery';
+
+$(document).ready(()=>{
+    $('h3')
+    .css({
+        color:"red",
+        fontSize:"55pt"
+    })
+    .click(()=>{
+        alert("Clicked")
+    })
+
+
+})
